2d330cf3030e72129d6c1eda207bc10a8018148a
Alessandro Chitolina <alekitto@gmail.com>
2020-11-06 22:45:36 +0100

    add getHeaders method on ResponseInterface

--- src/Requester/Response/ResponseInterface.php
+++ src/Requester/Response/ResponseInterface.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Solido\Atlante\Requester\Response;
 
+use Solido\Atlante\Http\HeaderBag;
+
 interface ResponseInterface
 {
     /**
@@ -12,6 +14,11 @@ interface ResponseInterface
      * @return object|string
      */
     public function getData();
+
+    /**
+     * Gets the HTTP headers received in the response.
+     */
+    public function getHeaders(): HeaderBag;
 
     /**
      * Gets the HTTP status code of the response.
