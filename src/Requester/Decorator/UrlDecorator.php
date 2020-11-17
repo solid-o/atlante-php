d4ab6c37126f3d2f084140cec18d81adb0c4f640
Alessandro Chitolina <alekitto@gmail.com>
2020-11-17 17:13:42 +0100

    fix url resolution in url decorator

--- src/Requester/Decorator/UrlDecorator.php
+++ src/Requester/Decorator/UrlDecorator.php
@@ -8,10 +8,11 @@ use Solido\Atlante\Requester\Request;
 
 use function http_build_query;
 use function parse_str;
-use function rtrim;
 use function Safe\array_replace_recursive;
 use function Safe\parse_url;
+use function Safe\substr;
 use function strpos;
+use function strrpos;
 use function strtr;
 
 class UrlDecorator implements DecoratorInterface
@@ -29,6 +30,9 @@ class UrlDecorator implements DecoratorInterface
 
         $parsedUrl = parse_url($url);
         $parsedBase = parse_url($this->baseUrl);
+        if (isset($parsedUrl['scheme'], $parsedUrl['host'], $parsedUrl['path'])) {
+            return new Request($request->getMethod(), $url, $request->getHeaders(), $request->getBody());
+        }
 
         $url = strtr('schema://auth@host:port/path?query#fragment', [
             'schema://' => ($scheme = $parsedBase['scheme'] ?? null) !== null ? $scheme . '://' : '',
@@ -36,32 +40,48 @@ class UrlDecorator implements DecoratorInterface
                 ($user . ':' . $pass . '@') : '',
             'host' => $parsedBase['host'] ?? '',
             ':port' => $parsedBase['port'] ?? null ? ':' . $parsedBase['port'] : '',
-            '/path?query' => (static function () use ($parsedUrl, $parsedBase): string {
-                $basePath = $parsedBase['path'] ?? null;
-                $baseQuery = $parsedBase['query'] ?? null;
+            '/path?query#fragment' => (static function () use ($parsedUrl, $parsedBase): string {
+                $basePath = $parsedBase['path'] ?? '/';
+                $baseQuery = $parsedBase['query'] ?? '';
+                $baseFragment = $parsedBase['fragment'] ?? '';
                 $urlPath = $parsedUrl['path'] ?? null;
                 $urlQuery = $parsedUrl['query'] ?? null;
+                $urlFragment = $parsedUrl['fragment'] ?? null;
 
-                if ($basePath !== null && $urlPath !== null) {
-                    // keep baseurl path if has any queries, combine otherwise (path prefix)
-                    $path = $baseQuery ? $basePath : rtrim($basePath, '/') . $urlPath;
+                $path = $basePath;
+                $query = $baseQuery;
+                $fragment = $baseFragment;
+                if ($urlPath !== null) {
+                    if (($urlPath[0] ?? null) === '/') {
+                        return $urlPath .
+                            (($urlQuery ?? '') !== '' ? '?' : '') . $urlQuery .
+                            (($urlFragment ?? '') !== '' ? '#' : '') . $urlFragment;
+                    }
+
+                    if ($basePath !== null) {
+                        $pos = strrpos($basePath, '/');
+                        $path = ($pos !== false ? substr($basePath, 0, $pos) : '') . '/' . $urlPath;
+                    }
+
+                    $query = $urlQuery ?? '';
+                    $fragment = $urlFragment ?? '';
                 } else {
-                    $path = $basePath . $urlPath;
+                    if ($urlQuery !== null) {
+                        $bq = $uq = [];
+                        parse_str($baseQuery, $bq);
+                        parse_str($urlQuery, $uq);
+                        $query = http_build_query(array_replace_recursive($bq, $uq));
+                    }
+
+                    if ($urlFragment !== null) {
+                        $fragment = $urlFragment;
+                    }
                 }
 
-                if ($baseQuery === null || $urlQuery === null) {
-                    $query = $baseQuery . $urlQuery;
-                } else {
-                    $bq = $uq = [];
-                    parse_str($baseQuery, $bq);
-                    parse_str($urlQuery, $uq);
-                    $query = http_build_query(array_replace_recursive($bq, $uq));
-                }
-
-                return (strpos($path, '/') === 0 ? '' : '/') . $path . ($query !== '' ? '?' : '') . $query;
+                return (strpos($path, '/') === 0 ? '' : '/') . $path .
+                    ($query !== '' ? '?' : '') . $query .
+                    ($fragment !== '' ? '#' : '') . $fragment;
             })(),
-            '#fragment' => ($fragment = $parsedUrl['fragment'] ?? $parsedBase['fragment'] ?? null) === null ? '' :
-                '#' . $fragment,
         ]);
 
         return new Request($request->getMethod(), $url, $request->getHeaders(), $request->getBody());
--- tests/Requester/Decorator/UrlDecoratorTest.php
+++ tests/Requester/Decorator/UrlDecoratorTest.php
@@ -31,18 +31,21 @@ class UrlDecoratorTest extends TestCase
 
     public static function provideDecorateCases(): Generator
     {
-        yield ['http://api.example.test', 'http://localhost/foo', 'http://api.example.test/foo'];
-        yield ['https://api.example.com', 'http://localhost/foo', 'https://api.example.com/foo'];
+        yield ['http://api.example.test', '/foo', 'http://api.example.test/foo'];
+        yield ['https://api.example.com', '/foo', 'https://api.example.com/foo'];
 
-        yield ['http://api.example.test/', 'http://localhost/foo/', 'http://api.example.test/foo/'];
-        yield ['https://api.example.com/foo', '/bar', 'https://api.example.com/foo/bar'];
+        yield ['http://api.example.test/', 'http://localhost/foo/', 'http://localhost/foo/'];
+        yield ['https://api.example.com/foo/', 'bar', 'https://api.example.com/foo/bar'];
+        yield ['https://api.example.com/foo', 'bar', 'https://api.example.com/bar'];
+        yield ['https://api.example.com/foo', '/bar', 'https://api.example.com/bar'];
         yield ['https://api.example.com', 'foo', 'https://api.example.com/foo'];
 
-        yield ['http://api.example.test/foo?test=1', '/foo', 'http://api.example.test/foo?test=1'];
+        yield ['http://api.example.test/foo?test=1', '/foo', 'http://api.example.test/foo'];
         yield ['http://api.example.test/foo?test=1', '/foo?test=2&c=a', 'http://api.example.test/foo?test=2&c=a'];
 
-        yield ['http://api.example.test/foo#frag', '/bar', 'http://api.example.test/foo/bar#frag'];
-        yield ['http://api.example.test/foo', '/bar#frag', 'http://api.example.test/foo/bar#frag'];
-        yield ['http://api.example.test/foo#no-frag', '/bar#frag', 'http://api.example.test/foo/bar#frag'];
+        yield ['http://api.example.test/foo/#frag', 'bar', 'http://api.example.test/foo/bar'];
+        yield ['http://api.example.test/foo/', 'bar#frag', 'http://api.example.test/foo/bar#frag'];
+        yield ['http://api.example.test/foo/#no-frag', 'bar#frag', 'http://api.example.test/foo/bar#frag'];
+        yield ['http://api.example.test/foo/#no-frag', '/bar#frag', 'http://api.example.test/bar#frag'];
     }
 }
